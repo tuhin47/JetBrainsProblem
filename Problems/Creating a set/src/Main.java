d75fdf3f7f274a9ddaf2ba988a187dbd0755a846
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-11-13 23:44:14 +0600

    SET 1 solved

---
+++ Problems/Creating a set/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        TreeSet<String> set = new TreeSet<>(List.of("Gamma", "Alpha", "Omega"));
+        System.out.println(set);
+    }
+}
